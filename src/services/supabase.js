b1be455d8a1d638bee0d47f17adcd7939c637e98
Michał Klepner <michal.klepner1@o2.pl>
2024-10-08 16:46:43 +0200

    created data base at supabase, checked functionality of that by console logging cabin at cabin page

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://sxhigdibbkhiczaltcwr.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4aGlnZGliYmtoaWN6YWx0Y3dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzODM3MDYsImV4cCI6MjA0Mzk1OTcwNn0.67Cukfbe2d11pzqsBYqjTMrg6DH0eqiMFo10EdiPJf4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
