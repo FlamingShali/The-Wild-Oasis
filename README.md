dad32db48bd6c804d0e9d102aa7a439e4c063a00
Michał Klepner <michal.klepner1@o2.pl>
2024-10-08 10:42:41 +0200

    added first styled components

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# React + Vite
+One on the biggest (if not biggest) project, made as a part of Jonas's Schmedtmann (original author) React course. 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -20,10 +15,12 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
         <Button>Check in</Button>
         <Button>Check Out</Button>
-
+        <Heading as="h2">Check In and Out</Heading>
+        <Input type="number" placeholder="Number of guests" />
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
